92e0619011cd8c524e557e7642b16a3755e5e644
Minjun Shin <ohgree@u.sogang.ac.kr>
2018-06-24 07:49:04 +0000

    changed shebang to properly execute python3

--- items/dgitemlist.py
+++ items/dgitemlist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Demonstration of ItemList class in the planned plugin DetectiveGame420"""
--- professions/dgprofession.py
+++ professions/dgprofession.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """Demonstration of DGProfession class in the planned plugin DetectiveGame420"""
